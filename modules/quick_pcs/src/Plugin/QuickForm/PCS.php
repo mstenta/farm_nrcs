5bd5ef390ab1acb6dcce8e31976b485e4631123f
Michael Stenta <mike@mstenta.net>
2023-01-23 12:41:23 -0500

    wip: pcs quick form
    @todo include other fields from worksheet header?
    @todo include more info from worksheet? or just link to worksheet?

---
+++ modules/quick_pcs/src/Plugin/QuickForm/PCS.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace Drupal\farm_nrcs_quick_pcs\Plugin\QuickForm;
+
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\farm_quick\Plugin\QuickForm\QuickFormBase;
+use Drupal\farm_quick\Traits\QuickLogTrait;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Pasture condition score quick form.
+ *
+ * @QuickForm(
+ *   id = "nrcs_pcs",
+ *   label = @Translation("Pasture condition score"),
+ *   description = @Translation("Record an observerd pasture condition score."),
+ *   helpText = @Translation("Use this form to record a pasture condition score as an observation."),
+ *   permissions = {
+ *     "create observation log",
+ *   }
+ * )
+ */
+class PCS extends QuickFormBase {
+
+  use QuickLogTrait;
+
+  /**
+   * The time service.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  protected $time;
+
+  /**
+   * Constructs a QuickFormBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MessengerInterface $messenger, TimeInterface $time) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $messenger);
+    $this->messenger = $messenger;
+    $this->time = $time;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('messenger'),
+      $container->get('datetime.time'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, string $id = NULL) {
+
+    // Pasture asset reference.
+    $form['asset'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Pasture'),
+      '#description' => $this->t('Which pasture is this an observation of?'),
+      '#target_type' => 'asset',
+      '#selection_handler' => 'views',
+      '#selection_settings' => [
+        'view' => [
+          'view_name' => 'farm_location_reference',
+          'display_name' => 'entity_reference',
+          'arguments' => [],
+        ],
+        'match_operator' => 'CONTAINS',
+      ],
+      '#required' => TRUE,
+    ];
+
+    // Date of observation.
+    $form['date'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Date'),
+      '#default_value' => date('Y-m-d', $this->time->getRequestTime()),
+      '#required' => TRUE,
+    ];
+
+    // Create a score field for each indicator.
+    $indicators = [
+      'desirable_plants' => [
+        'title' => $this->t('Percent Desirable Plants'),
+      ],
+      'percent_legume' => [
+        'title' => $this->t('Percent Legume by Dry Weight'),
+      ],
+      'live_plant_cover' => [
+        'title' => $this->t('Live (includes dormant) Plant Cover'),
+      ],
+      'plant_diversity' => [
+        'title' => $this->t('Plant Diversity by Dry Weight'),
+      ],
+      'plant_residue' => [
+        'title' => $this->t('Plant Residue and Litter as Soil Cover'),
+      ],
+      'grazing_utilization' => [
+        'title' => $this->t('Grazing Utilization and Severity'),
+      ],
+      'livestock_concentration' => [
+        'title' => $this->t('Livestock Concentration Areas'),
+      ],
+      'soil_compaction' => [
+        'title' => $this->t('Soil Compaction and Soil Regenerative Features'),
+      ],
+      'plant_vigor' => [
+        'title' => $this->t('Plant Vigor'),
+      ],
+      'erosion' => [
+        'title' => $this->t('Erosion'),
+      ],
+    ];
+    $score_options = range(1, 5);
+    $form['indicators'] = ['#tree' => TRUE];
+    foreach ($indicators as $name => $indicator) {
+      $form['indicators'][$name] = [
+        '#type' => 'select',
+        '#title' => $indicator['title'],
+        '#options' => array_combine($score_options, $score_options),
+        '#required' => TRUE,
+        '#ajax' => [
+          'callback' => [$this, 'overallScoreCallback'],
+          'wrapper' => 'pcs-score',
+        ],
+      ];
+    }
+
+    // Summarize the overall pasture condition score when indicators are
+    // updated.
+    $values = $form_state->getValue('indicators');
+    if (!empty($values)) {
+      $score = 0;
+      foreach ($values as $value) {
+        if (!empty($value)) {
+          $score += $value;
+        }
+      }
+    }
+    else {
+      $score = 'N/A';
+    }
+    $form['score'] = [
+      '#tree' => TRUE,
+      '#prefix' => '<div id="pcs-score">',
+      '#suffix' => '</div>',
+    ];
+    $form['score']['value'] = [
+      '#type' => 'value',
+      '#value' => $score,
+    ];
+    $form['score']['markup'] = [
+      '#type' => 'markup',
+      '#markup' => '<strong>' . $this->t('Overall Pasture Condition Score') . ': ' . $score . '</strong>',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // Get the overall score value.
+    $score = $form_state->getValue(['score', 'value']);
+
+    // Draft an observation log.
+    $log = [
+      'name' => $this->t('Pasture condition score'),
+      'type' => 'observation',
+      'timestamp' => strtotime($form_state->getValue('date')),
+      'asset' => $form_state->getValue('asset'),
+      'quantity' => [
+        [
+          'measure' => 'rating',
+          'value' => $score,
+          'label' => $this->t('Overall Score'),
+        ],
+      ],
+      'status' => 'done',
+    ];
+
+    // Add quantity measurements for each indicator.
+    $indicator_values = $form_state->getValue('indicators');
+    foreach ($indicator_values as $name => $value) {
+      $log['quantity'][] = [
+        'measure' => 'rating',
+        'value' => $value,
+        'label' => $form['indicators'][$name]['#title'],
+      ];
+    }
+
+    // Create the log.
+    $this->createLog($log);
+  }
+
+  /**
+   * Ajax callback for the overall pasture condition score.
+   */
+  public function overallScoreCallback(array $form, FormStateInterface $form_state) {
+    return $form['score'];
+  }
+
+}
